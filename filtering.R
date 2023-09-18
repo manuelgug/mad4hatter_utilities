2bd8acdd3ec0d4125ad8fb14e944de74bab22d67
manuelgug <36465310+manuelgug@users.noreply.github.com>
2023-09-18 15:37:38 +0200

    Update filtering.R

--- filtering.R
+++ filtering.R
@@ -6,9 +6,9 @@ suppressPackageStartupMessages(library(gridExtra))
 
 # Define and parse command-line arguments
 option_list <- list(
-  make_option(c("--allele_table"), type = "character", help = "Path to the allele table", default ="test_data/allele_data.txt"), #QUITAR DEFAULT Y PONER default = NULL, 
-  make_option(c("--microhaps_table"), type = "character", help = "Path to the resmarkers microhap table", default = "test_data/resmarker_microhap_table.txt"), #QUITAR DEFAULT Y PONER default = NULL, 
-  make_option(c("--resmarkers_table"), type = "character", help = "Path to the resmarkers table", default = "test_data/resmarker_table.txt"), #QUITAR DEFAULT Y PONER default = NULL, 
+  make_option(c("--allele_table"), type = "character", help = "Path to the allele table", default ="TES22_NextSeq01_RESULTS_v0.1.8/allele_data.txt"), #QUITAR DEFAULT Y PONER default = NULL, 
+  make_option(c("--microhaps_table"), type = "character", help = "Path to the resmarkers microhap table", default = "TES22_NextSeq01_RESULTS_v0.1.8/resistance_marker_module/resmarker_microhap_table.txt"), #QUITAR DEFAULT Y PONER default = NULL, 
+  make_option(c("--resmarkers_table"), type = "character", help = "Path to the resmarkers table", default = "TES22_NextSeq01_RESULTS_v0.1.8/resistance_marker_module//resmarker_table.txt"), #QUITAR DEFAULT Y PONER default = NULL, 
   make_option(c("--CFilteringMethod"), type = "character", default = "amp_max", help = "Contaminants filtering method: global_max, global_q95, amp_max, amp_q95"),
   make_option(c("--MAF"), type = "numeric", default = 0, help = "Minimum allele frequency; default 0"),
   make_option(c("--exclude_file"), type = "character", default = NULL, help = "Path to the file containing sampleIDs to exclude"),
@@ -421,12 +421,6 @@ if (!is.null(resmarkers_table)){
       resmarkers_filtered <- resmarkers[resmarkers$Reads > CFilteringMethod, ] #single threshold for all amplicons
     }
   }
-    
-  #### NEED TO ADD LOCUS COLUMN TO CACLULATE MAF THRESHOLD OF RESMARKERS!!!   #### 
-  
-  amp_res_eq<-read.csv("resources/amplicons_resmarkers_equivalence.csv") 
-  
-  resmarkers_filtered<-merge(resmarkers_filtered, amp_res_eq, by = "resmarker", all.x = TRUE)
   
     # calculate allele counts and allele freqs
     resmarkers_filtered <- resmarkers_filtered %>%
